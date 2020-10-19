a797593522d889e8aaeafc72cff0dd0f8897b131
nubesdominum <ngainey@gmail.com>
2020-10-19 07:23:22 -0400

    Did some stuff with the things

--- windows/scripts/unused/Enable-Winrm.ps1
+++ windows/scripts/unused/Enable-Winrm.ps1
@@ -1,7 +1,3 @@
-$NetworkListManager = [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]"{DCB00C01-570F-4A9B-8D69-199FDBA5723B}"))
-$Connections = $NetworkListManager.GetNetworkConnections()
-$Connections | ForEach-Object { $_.GetNetwork().SetCategory(1) }
-
 Enable-PSRemoting -Force
 winrm quickconfig -q
 winrm quickconfig -transport:http
